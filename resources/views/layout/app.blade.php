b9ceedf6a3d95946c06e21c1c6f1159083a7298e
Fitri Zuyina Nur Azizah <nuranoer7@gmail.com>
2023-08-28 06:49:07 +0700

    insert template admin

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -6,14 +6,14 @@
 	<meta name="author" content="SemiColonWeb" />
 
 	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,400i,700|Poppins:300,400,500,600,700|PT+Serif:400,400i&display=swap" rel="stylesheet" type="text/css" />
-	<link rel="stylesheet" href="{{ asset ('css/bootstrap.css')" type="text/css" />
-	<link rel="stylesheet" href="{{ asset ('css/style.css')" type="text/css" />
-	<link rel="stylesheet" href="{{ asset ('css/dark.css')" type="text/css" />
-	<link rel="stylesheet" href="{{ asset ('css/font-icons.css')" type="text/css" />
-	<link rel="stylesheet" href="{{ asset ('css/animate.css')" type="text/css" />
-	<link rel="stylesheet" href="{{ asset ('css/magnific-popup.css')" type="text/css" />
+	<link rel="stylesheet" href="{{ asset ('css/bootstrap.css') }}" type="text/css" />
+	<link rel="stylesheet" href="{{ asset ('css/style.css') }}" type="text/css" />
+	<link rel="stylesheet" href="{{ asset ('css/dark.css') }}" type="text/css" />
+	<link rel="stylesheet" href="{{ asset ('css/font-icons.css') }}" type="text/css" />
+	<link rel="stylesheet" href="{{ asset ('css/animate.css') }}" type="text/css" />
+	<link rel="stylesheet" href="{{ asset ('css/magnific-popup.css') }}" type="text/css" />
 
-	<link rel="stylesheet" href="{{ asset ('css/custom.css')" type="text/css" />
+	<link rel="stylesheet" href="{{ asset ('css/custom.css') }}" type="text/css" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
 
 	<title>Katalog Griya Batik Wakluang</title>
@@ -63,10 +63,10 @@
 
 	<div id="gotoTop" class="icon-angle-up"></div>
 
-	<script src="{{ asset ('js/jquery.js')"></script>
-	<script src="{{ asset ('js/plugins.min.js')"></script>
+	<script src="{{ asset ('js/jquery.js') }}"></script>
+	<script src="{{ asset ('js/plugins.min.js') }}"></script>
 
-	<script src="{{ asset ('js/functions.js')"></script>
+	<script src="{{ asset ('js/functions.js') }}"></script>
 
 	<script>
 		jQuery(document).ready( function($){
